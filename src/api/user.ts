66301d0ecf9d291e48fbdeab860fd868d970123f
dz <wpqkfalcy@gmail.com>
2025-02-04 18:52:21 +0900

    feat  - api 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -5,7 +5,7 @@ const url = 'http://14.4.119.31:8081/api/users';
 export async function findUser(id: string) {
   //
   return await axios
-    .get(`${url}/${id}?userId=${id}`)
+    .get(`${url}/${id}`)
     .then((response) => response.data)
     .catch(console.error);
 }
